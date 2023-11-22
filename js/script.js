2b11b630a990a76b6e5e7caa38c2559cab0aa65a
samettekin01 <samet.tekin.01@gmail.com>
2023-11-22 16:18:38 +0300

    added bitwise

--- js/script.js
+++ js/script.js
@@ -2,25 +2,25 @@ const binaryDiv = document.querySelector(".binary")
 const decimalDiv = document.querySelector(".decimal")
 const btns = document.querySelectorAll(".btn")
 let opStatus = true
-const binaryCalculator = [{
+
+const binaryCalculator = {
     total: "",
     operator: ""
-}]
-const regOpControlEnd = /[+\/*-]$/
+}
+//Buttonları dinliyor.
 btns.forEach(btn => btn.addEventListener("click", e => {
     const inputString = e.target.textContent;
+    
     if (inputString == "0" || inputString == "1") {
         binaryDiv.textContent += inputString
-        binaryCalculator[0].total = binaryDiv.textContent
+        binaryCalculator.total = binaryDiv.textContent
         decimal()
     }
-    if (inputString == "C") {
-        reset()
-    }
+
     if (e.target.id == "op") {
         if (!binaryDiv.textContent == "") {
             if (opStatus) {
-                binaryCalculator[0].operator = inputString
+                binaryCalculator.operator = inputString
                 binaryDiv.textContent += inputString
                 opStatus = false
             } else {
@@ -28,37 +28,84 @@ btns.forEach(btn => btn.addEventListener("click", e => {
             }
         }
     }
-    if (inputString == "=") {
-        equal()
-    }
-}))
 
+    if (inputString == "=") equal()
+    if (inputString == "C") reset()
+}))
+//Seçilen matematisel veya bitwise operatörüne göre işlem yapıyor
 const equal = () => {
     let result = 0;
-    const find = binaryCalculator[0].total.indexOf(binaryCalculator[0].operator)
-    const firstValue = parseInt(binaryCalculator[0].total.slice(0, find), 2)
-    const secondValue = parseInt(binaryCalculator[0].total.slice(find + 1, binaryCalculator[0].total.length), 2)
+    const decimalConvert = new decimalConverter()
+    const decimalFirstValue = decimalConvert.firstValue()
+    const decimalSecondValue = decimalConvert.secondValue()
 
-    binaryCalculator[0].operator == "+" ? result = Number(firstValue) + Number(secondValue) : ""
-    binaryCalculator[0].operator == "-" ? result = Number(firstValue) - Number(secondValue) : ""
-    binaryCalculator[0].operator == "*" ? result = Number(firstValue) * Number(secondValue) : ""
-    binaryCalculator[0].operator == "/" ? result = Number(firstValue) / Number(secondValue) : ""
-
+    switch (binaryCalculator.operator) {
+        case "+":
+            result = decimalFirstValue + decimalSecondValue
+            break;
+        case "-":
+            result = decimalFirstValue - decimalSecondValue
+            break;
+        case "*":
+            result = decimalFirstValue * decimalSecondValue
+            break;
+        case "/":
+            result = decimalFirstValue / decimalSecondValue
+            break;
+        case "&":
+            result = decimalFirstValue & decimalSecondValue
+            break;
+        case "|":
+            result = decimalFirstValue | decimalSecondValue
+            break;
+        case "^":
+            result = decimalFirstValue ^ decimalSecondValue
+            break;
+        case "~":
+            result = ~decimalFirstValue
+            break;
+        case "<<":
+            result = decimalFirstValue << decimalSecondValue
+            break;
+        case ">>":
+            result = decimalFirstValue >> decimalSecondValue
+            break;
+        case ">>>":
+            result = decimalFirstValue >>> decimalSecondValue
+            break;
+        default:
+            result = decimalFirstValue
+    }
     binaryDiv.textContent = result.toString(2)
     decimalDiv.textContent = result
+    binaryCalculator.total = result.toString(2)
+    binaryCalculator.operator = ""
     opStatus = true
-
 }
+//Binary değeri decimal değerine çeviriyor.
+const decimal = () => {
+    const decimalConvert = new decimalConverter()
+    const firstValue = decimalConvert.firstValue()
+    const secondValue = decimalConvert.secondValue()
+    decimalDiv.textContent = `${firstValue ? firstValue : ""} ${binaryCalculator.operator ? binaryCalculator.operator : ""} ${secondValue ? secondValue : ""}`
+    console.log(firstValue, secondValue)
+}
+//tüm değerleri sıfırlıyor
 const reset = () => {
     binaryDiv.textContent = ""
-    binaryCalculator[0].total = ""
-    binaryCalculator[0].operator = ""
+    binaryCalculator.total = ""
+    binaryCalculator.operator = ""
     decimalDiv.textContent = ""
 }
-
-const decimal = () => {
-    const find = binaryCalculator[0].total.indexOf(binaryCalculator[0].operator)
-    const firstValue = parseInt(binaryCalculator[0].total.slice(0, find), 2)
-    const secondValue = parseInt(binaryCalculator[0].total.slice(find + 1, binaryCalculator[0].total.length), 2)
-    decimalDiv.textContent = `${firstValue ? firstValue : ""} ${binaryCalculator[0].operator ? binaryCalculator[0].operator : ""} ${secondValue ? secondValue : ""}`
+//binaryCalculator object içindeki total özelliğindeki 1. ve 2. binary değerleri buluyor.
+class decimalConverter {
+    constructor() {
+        this.find = binaryCalculator.operator !== "" ? binaryCalculator.total.indexOf(binaryCalculator.operator) : -1
+    }
+    firstValue() {
+        return this.find !== -1 ? parseInt(binaryCalculator.total.slice(0, this.find), 2) : parseInt(binaryCalculator.total, 2)
+    }
+    secondValue() {
+        return binaryCalculator.operator !== "" ? parseInt(binaryCalculator.total.slice(this.find + 1, binaryCalculator.total.length).replace(/\D/g, ""), 2) : ""
+    }
 }
